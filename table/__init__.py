b4b6efcbbca114656a73d913d1f58044f250cc93
Thomas S Hatch <thatch45@gmail.com>
2014-01-23 13:11:54 -0700

    prevent json from changing values to unicode

--- table/__init__.py
+++ table/__init__.py
@@ -21,6 +21,40 @@ except ImportError:
     HAS_MSGPACK = False
 
 
+def decode_list(data):
+    '''
+    JSON decodes as unicode, Jinja needs bytes...
+    '''
+    rv = []
+    for item in data:
+        if isinstance(item, unicode):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = decode_list(item)
+        elif isinstance(item, dict):
+            item = decode_dict(item)
+        rv.append(item)
+    return rv
+
+
+def decode_dict(data):
+    '''
+    JSON decodes as unicode, Jinja needs bytes...
+    '''
+    rv = {}
+    for key, value in data.iteritems():
+        if isinstance(key, unicode):
+            key = key.encode('utf-8')
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        elif isinstance(value, list):
+            value = decode_list(value)
+        elif isinstance(value, dict):
+            value = decode_dict(value)
+        rv[key] = value
+    return rv
+
+
 def now():
     '''
     Return now as a date list
@@ -94,7 +128,7 @@ class Serial(object):
         '''
         Load JSON string
         '''
-        return json.loads(data)
+        return json.loads(data, object_hook=decode_dict)
 
     def dumps_msgpack(self, data):
         '''
